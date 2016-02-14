6518384625ca294c13fa5eee2924b180490974be
Sam Martin <samjackmartin@gmail.com>
2016-02-14 00:12:56 +0000

    Now listens on 1433 reliably

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'Apache v2.0'
 description 'Facilitates the setup of a multi-node test setup using ' \
       'MS SQL Server 2014 in Test Kitchen'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 supports 'windows'
 
 depends 'boxstarter'
+depends 'firewall'
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,14 +1,53 @@
 include_recipe 'boxstarter::default'
+include_recipe 'firewall::default'
 
 boxstarter "boxstarter install SQL Server" do
   password node['test_kitchen_mssql_helpers']['chef_client_user_password']
   disable_reboots false
   code <<-EOH
-  	
-	[Environment]::SetEnvironmentVariable("chocolateyInstallArguments",'/SECURITYMODE=SQL /SAPWD="#{node['test_kitchen_mssql_helpers']['sa_password']}"',"Machine")
+  	$SQLArgs = '/SECURITYMODE=SQL /SAPWD="#{node['test_kitchen_mssql_helpers']['sa_password']}" /TCPENABLED=1'
+	[Environment]::SetEnvironmentVariable("chocolateyInstallArguments",$SQLArgs,"Machine")
   	cinst mssqlserver2014express
-  	
-  	Install-WindowsFeature Net-Framework-Core
-  	cinst mssqlservermanagementstudio2014express
+
   EOH
 end
+
+powershell_script 'Set IpAll TCP 1433 listener in MSSQL' do 
+  code <<-EOH
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
+$mc = new-object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer
+$tcp = $mc.GetSmoObject("ManagedComputer[@Name='$env:computername']/ ServerInstance[@Name='SQLEXPRESS']/ServerProtocol[@Name='Tcp']")
+
+if($tcp.IPAddresses['IPAll'].IPAddressProperties['TcpPort'].value -ne '1433'){
+	$tcp.IPAddresses | %{$_.IPAddressProperties['TcpPort'].value=''}
+	$tcp.IPAddresses | %{$_.IPAddressProperties['TcpDynamicPorts'].value=''}
+	$tcp.IPAddresses['IPAll'].IPAddressProperties['TcpPort'].value='1443'
+	$tcp.IsEnabled = $true
+	$tcp.alter()
+	Restart-Service 'MSSQL$SQLEXPRESS' -Force
+}
+
+try{
+	New-Object System.Net.Sockets.TCPClient -ArgumentList localhost, 1433
+}catch{
+	$tcp.IPAddresses['IPAll'].IPAddressProperties['TcpPort'].value=''
+	$tcp.IsEnabled = $true
+	$tcp.alter()
+	Restart-Service 'MSSQL$SQLEXPRESS' -Force
+
+	$tcp.IPAddresses['IPAll'].IPAddressProperties['TcpPort'].value='1433'
+	$tcp.IsEnabled = $true
+	$tcp.alter()
+	Restart-Service 'MSSQL$SQLEXPRESS' -Force
+}
+  EOH
+end
+
+firewall_rule 'mssql' do
+  port     1433
+  protocol :tcp
+  position 1
+  command   :allow
+end
+
